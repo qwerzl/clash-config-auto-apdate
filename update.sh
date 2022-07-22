ce8239d94524b27d9d36a2d40f79165a3ca27040
qwerzl <46770502+qwerzl@users.noreply.github.com>
2022-07-22 21:49:24 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LINK=你的订阅链接
+LINK=订阅链接
 mv ./config.yaml ./config.yaml.bak
 
 curl -L -o config.yaml $LINK
@@ -8,6 +8,7 @@ CURLSTATUS=$?
 
 chmod +x ./yq
 
+# 修改配置
 ./yq -i '
   .port = 7890 |
   .allow-lan = true
@@ -18,9 +19,11 @@ if [ $CURLSTATUS -eq 0 ] || [ $YQSTATUS -eq 0 ]
 then
   if cmp --silent -- "$DIRECTORY/config.yaml.bak" "$DIRECTORY/config.yaml"
   then
+    echo "# $(TZ=Asia/Shanghai date)" >> config.yaml
     echo "Clash config updated successfully" "Nothing changed"
     rm ./config.yaml.bak
   else
+    echo "# $(TZ=Asia/Shanghai date)" >> config.yaml
     echo "Clash config updated successfully" "File has changed"
     rm ./config.yaml.bak
   fi
@@ -28,4 +31,5 @@ else
   echo "Clash config updated failed" "cURL: $CURLSTATUS  yq: $YQSTATUS"
   rm ./config.yaml
   mv ./config.yaml.bak ./config.yaml
+  touch ./config.yaml.bak
 fi
